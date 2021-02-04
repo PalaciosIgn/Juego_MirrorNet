689a983fbcc63b759018aeaead7e618f8678b7eb
PalaciosIgn <72041740+PalaciosIgn@users.noreply.github.com>
2021-02-04 15:59:39 -0300

    subir

---
+++ JuegoMirrorNetwork/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float velocidad = 1;
+    private TextMesh tm = null;
+    private Text ui = null;
+
+    public void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+        SetPlayerCaption("x");
+        SetTitle("Titulo del juego");
+    }
+    public void Update()
+    {
+        //move
+        float movH = CrossPlatformInputManager.GetAxis("Horizontal")*velocidad;
+        float movV = CrossPlatformInputManager.GetAxis("Vertical")*velocidad;
+       rb.AddForce(movH, 0, movV);
+    }
+
+    public void SetPlayerCaption(string caption)
+    {
+        if(tm == null)
+        {
+            for(int i = 0; i < this.transform.childCount; i++)
+            {
+                if (this.transform.GetChild(i).name == "Caption")
+                {
+                    tm = this.transform.GetChild(i).GetComponent<TextMesh>();
+                }
+            }
+        }
+        if (tm != null)
+        {
+            tm.text = caption;
+        }
+        else
+        {
+            tm.text = "err";
+        }
+    }
+    public void SetTitle(string caption)
+    {
+        if (ui == null)
+        {
+            ui = GameObject.Find("txtTitle").GetComponent<Text>();
+        }
+        if (ui != null)
+        {
+            ui.text = caption;
+        }
+    }
+}
---
+++ JuegoMirrorNetwork/Assets/Scripts/TurnOffRemotePlayer.cs
@@ -0,0 +1,25 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TurnOffRemotePlayer : NetworkBehaviour
+{
+    private void Start()
+    {
+        string id = string.Format("{0}", this.netId);
+        Player scr = this.GetComponent<Player>();
+
+
+        if (this.isLocalPlayer == true)
+        {
+            scr.enabled = true;
+            scr.SetPlayerCaption(id);
+            scr.SetTitle("Multi Player #" + id);
+        }
+        else
+        {
+            scr.SetPlayerCaption(id);
+            scr.enabled = false;
+        }
+    }
+}
